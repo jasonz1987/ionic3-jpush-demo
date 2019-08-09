c966d1a013c618a6e63fadf1649400b747776c54
Jason.z <ccnuzxg@gmail.com>
2019-08-09 09:45:50 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { JPush } from '@jiguang-ionic/jpush';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,jpush: JPush) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      jpush.init();
+      jpush.setDebugMode(true);
+
+      document.addEventListener('jpush.receiveRegistrationId', function (event) {
+        console.log(event['registrationId']);
+      }, false)
+
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { JPush } from '@jiguang-ionic/jpush';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,116 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private jpush: JPush) {
   }
+
+  getRegistrationID() {
+
+    this.jpush.getRegistrationID().then(id=>{
+      console.log(id);
+      alert(id);
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+
+  setAlias(alias) {
+    this.jpush.setAlias({ sequence: 1, alias: alias }).then( (result) => {
+      var sequence = result.sequence
+      var alias = result.alias
+    }).catch((error) => {
+      var sequence = error.sequence
+      var errorCode = error.code
+    })
+  }
+
+  getAlias() {
+    this.jpush.getAlias({ sequence: 1 }).then(
+      (result) => {
+        var sequence = result.sequence
+        var alias = result.alias
+        alert(result.alias);
+      }).catch ((error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  deleteAlias() {
+    this.jpush.deleteAlias({ sequence: 1 }).then(
+      (result) => {
+        var sequence = result.sequence
+      }).catch( (error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  setTags(tags) {
+    this.jpush.setTags({ sequence: 1, tags: tags }).then(
+      (result) => {
+        var sequence = result.sequence
+        var tags = result.tags  // 数组类型
+      }).catch((error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  addTags(tags) {
+    this.jpush.addTags({ sequence: 1, tags: tags }).then(
+      (result) => {
+        var sequence = result.sequence
+        var tags = result.tags  // 数组类型
+      }).catch((error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  deleteTags(tags) {
+    this.jpush.deleteTags({ sequence: 1, tags: tags }).then(
+      (result) => {
+        var sequence = result.sequence
+      }).catch((error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  cleanTags() {
+    this.jpush.cleanTags({ sequence: 1 }).then(
+      (result) => {
+        var sequence = result.sequence
+      }).catch((error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  getAllTags() {
+    this.jpush.getAllTags({ sequence: 1 }).then(
+      (result) => {
+        var sequence = result.sequence
+        alert(JSON.stringify(result));
+      }).catch((error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+  checkTagBindState(tag) {
+    this.jpush.checkTagBindState({ sequence: 1, tag: tag }).then(
+      (result) => {
+        var sequence = result.sequence
+        alert(JSON.stringify(result));
+      }).catch( (error) => {
+        var sequence = error.sequence
+        var errorCode = error.code
+      })
+  }
+
+
+
 
 }
